02ba23272712dcdc531696bcc2e73d76370f0f0c
Marco Fura <mfura@kataricorp.com>
2024-06-15 21:40:39 -0400

    feat: add anova table for full model and AIC model

--- aprendizaje-supervizado/laboratorio_1.r
+++ aprendizaje-supervizado/laboratorio_1.r
@@ -43,5 +43,49 @@ summary(model_aic)
 modelo_bic <- step(lm(involact ~ ., data = chicago),direction = "both" ,trace = 1000, k = log(length(chicago$involact)))
 summary(modelo_bic)
 # En este caso vemos que usando los criterios de AIC y BIC se seleccionan las mismas variables
+# Conclusiones:
+# - Con el uso de los criterios AIC y BIC ambos me consideran solo 4 variables como significativas para la elaboración del modelo lineal
 
 # 3. Significancia del Modelo
+anova(modelo_completo)
+anova(modelo_aic)
+anova(modelo_bic)
+anova(modelo_completo, modelo_aic)
+# Ambos modelos son significativos
+# Existen 2 covariables que no son significativas: volact y income
+# El modelo lineal generado ocupando solo las 4 variables significativas tiene un ajuste muy similar al modelo completo. Es decir remover las 2 variables menos significativas no genera una mejora significativa en el ajuste del modelo.
+
+
+# 4. MÉTODOS DE REGRESION CONTRAIDAS
+
+# - REGRESION RIDGE
+# Seleccionar el valor de lambda
+lambda <- seq(0, 1, 0.1)
+ridge <- cv.glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 0, lambda = lambda)
+plot(ridge)
+best_lambda <- ridge$lambda.min
+best_lambda
+ridge_model <- glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 0, lambda = best_lambda)
+coef(ridge_model)
+# - REGRESION LASSO
+lasso <- cv.glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 1, lambda = lambda)
+plot(lasso)
+best_lambda <- lasso$lambda.min
+best_lambda
+lasso_model <- glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 1, lambda = best_lambda)
+coef(lasso_model)
+# - REGRESION ELASTIC NET
+elastic_net <- cv.glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 0.5, lambda = lambda)
+plot(elastic_net)
+best_lambda <- elastic_net$lambda.min
+best_lambda
+elastic_net_model <- glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 0.5, lambda = best_lambda)
+coef(elastic_net_model)
+
+# 5. PREDICCION INDIVIDUAL Y DE LA MEDIA
+# Modelos lineales AIC y el completo
+# Predicción individual
+# Predicción de la media
+# Modelos Ridge, Lasso y Elastic Net
+# Predicción individual
+# Predicción de la media
