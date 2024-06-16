ca641c8b1c1aa88b5fcf4023cfa1cb5c19c0dfc4
Marcos Reiman <mreimand@estudiante.uc.cl>
2024-06-16 01:16:10 -0400

    se agrega comentarios de los resultados en los modelos

--- aprendizaje-supervizado/laboratorio_1.r
+++ aprendizaje-supervizado/laboratorio_1.r
@@ -1,3 +1,4 @@
+
 # Instalar faraway
 install.packages("faraway")
 # Cargar faraway
@@ -6,6 +7,7 @@ library(faraway)
 data(chicago)
 ?faraway::chicago
 
+chicago
 # 1. ANALISIS DESCRIPTIVO de las variables de la base de datos. Incluir indicadores y gráficos.
 # Columnas
 names(chicago)
@@ -32,13 +34,43 @@ boxplot(chicago$income)
 pairs(chicago)
 nrow(chicago)
 
-# 2. SELECCION DE VARIABLES
+# 2. SELECCION DE VARIABLES MAS SIGNIFICATIVAS
 # Modelo usando todas las variables
 modelo_completo <- lm(involact ~ ., data = chicago)
 summary(modelo_completo)
+
+#Comentarios de resultados****
+#Residual standard error: 0.3387 on 40 degrees of freedom
+#Multiple R-squared:  0.7517,    Adjusted R-squared:  0.7144
+#F-statistic: 20.18 on 6 and 40 DF,  p-value: 1.072e-10
+#DETALLE
+#Residual standard error: 0.3387 --> Esto mide el error estándar , mide la calida de los predictores del modelo
+#Multiple R-squared:  0.7517 --> el 75.17%  de la variabilidad se explica por las variables independientes
+#Adjusted R-squared:  0.7144 --> es un 71.44% al ser más bajo que el anterior indica que hay algun variable predictora que no agregar mucha información 
+
+
+
+
 # Criterio de Información de Akaike (AIC)
 modelo_aic <- step(lm(involact ~ ., data = chicago), direction = "both", trace = 1000)
-summary(model_aic)
+summary(modelo_aic)
+#Resultado *****
+#Hace 3 iteraciones 
+# Start:  AIC=-95.34  <-- con todas las variables  --> involact ~ race + fire + theft + age + volact + income
+# Step:  AIC=-97.18   <-- quita la variable volact --> involact ~ race + fire + theft + age + income
+# Step:  AIC=-98.5    <-- quita la variable income --> involact ~ race + fire + theft + age
+# las variables descartas (volact,income) en las iteraciones
+# El resultado del modelo 
+# Residual standard error: 0.3335 on 42 degrees of freedom
+# Multiple R-squared:  0.7472,    Adjusted R-squared:  0.7231
+# F-statistic: 31.03 on 4 and 42 DF,  p-value: 4.799e-12
+# Residual standard error: 0.3335 <-- el bajo error residual 
+
+
+
+
+
+
 # Criterio de Información Bayesiano (BIC)
 modelo_bic <- step(lm(involact ~ ., data = chicago),direction = "both" ,trace = 1000, k = log(length(chicago$involact)))
 summary(modelo_bic)
@@ -54,11 +86,14 @@ anova(modelo_completo, modelo_aic)
 # Ambos modelos son significativos
 # Existen 2 covariables que no son significativas: volact y income
 # El modelo lineal generado ocupando solo las 4 variables significativas tiene un ajuste muy similar al modelo completo. Es decir remover las 2 variables menos significativas no genera una mejora significativa en el ajuste del modelo.
-
+# El modelo no mejora significativamente al quitar las variables no significativas.
 
 # 4. MÉTODOS DE REGRESION CONTRAIDAS
 
 # - REGRESION RIDGE
+
+install.packages("glmnet")
+library(glmnet)
 # Seleccionar el valor de lambda
 lambda <- seq(0, 1, 0.1)
 ridge <- cv.glmnet(as.matrix(chicago[, -1]), chicago$involact, alpha = 0, lambda = lambda)
