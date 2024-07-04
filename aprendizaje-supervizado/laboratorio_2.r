3c130310df72475bcd7863ae0eefa24adfb094e0
Marco Fura <mjfura27@gmail.com>
2024-07-03 23:47:31 -0400

    refactor: add valid data in cv simple when the model is tested

--- aprendizaje-supervizado/laboratorio_2.r
+++ aprendizaje-supervizado/laboratorio_2.r
@@ -1,8 +1,10 @@
-# CARGAR LIBRERIAS
+# Instalar librerias
 install.packages("splitTools")
 install.packages("rpart.plot")
 install.packages("caret")
 install.packages("randomForest")
+# !Instalar librerias
+# Cargar librerias
 library(faraway)
 library(splitTools)
 library(rpart)
@@ -10,6 +12,7 @@ library(rpart.plot)
 library(caret)
 library(randomForest)
 library(pROC)
+# !Cargar librerias
 
 # PREGUNTAS PARTE 02
 # 1. De acuerdo al contexto del conjunto de datos,
@@ -105,14 +108,21 @@ modelo_knn <- train(
     method = "knn"
 )
 prediccion_knn <- predict(modelo_knn, newdata = valid_x)
-matrix_confusion <- confusionMatrix(prediccion_knn, valid_y_factor)
+matrix_confusion <- confusionMatrix(prediccion_knn, valid_y_factor,mode="everything")
 f1_score_knn <- calcular_f1_score(matrix_confusion)
 print(f1_score_knn)
 # 0.9666667
 # Modelo Arbol de Clasificacion
 data_train_factor <- cbind(train_x,Class=train_y_factor)
-arbol_decision <- rpart(Class ~ ., data = data_train_factor,method = "class")
+control <- rpart.control(minsplit = 20, cp = 0.005)
+arbol_decision <- rpart(Class ~ ., data = data_train_factor,
+                        method = "class",
+                        control = control)
+rpart::plotcp(arbol_decision)
+rpart::printcp(arbol_decision)
 rpart.plot(arbol_decision)
+
+
 predictions <- predict(arbol_decision, valid_x, type = "class")
 matrix_confusion <- confusionMatrix(predictions, valid_y_factor)
 f1_score_arbol <- calcular_f1_score(matrix_confusion)
@@ -129,6 +139,10 @@ print(f1_score_rf)
 # tanto el modelo KNN como el random forest tienen los valores más altos
 
 # 2.b. Testear Modelo escogido
+modelo_knn <- train(
+    x = rbind(train_x,valid_x), y = c(train_y_factor,valid_y_factor),
+    method = "knn"
+)
 predictions_test <- predict(modelo_knn, test_x)
 matrix_confusion <- confusionMatrix(predictions_test, test_y_factor)
 f1_score_test <- calcular_f1_score(matrix_confusion)
@@ -158,7 +172,7 @@ predicciones_knn <- modelo_knn$pred
 matrix_confusion_knn <- confusionMatrix(predicciones_knn$pred, predicciones_knn$obs)
 f1_score_knn <- calcular_f1_score(matrix_confusion_knn)
 print(f1_score_knn)
-# 0.9823812
+# 0.9715818
 
 # Arboldes de clasificacion
 arbol_decision <- train(
@@ -170,7 +184,7 @@ predicciones_arbol <- arbol_decision$pred
 matrix_confusion_arbol <- confusionMatrix(predicciones_arbol$pred, predicciones_arbol$obs)
 f1_score_arbol <- calcular_f1_score(matrix_confusion_arbol)
 print(f1_score_arbol)
-# 0.91280052
+# 0.9172853
 
 # Random Forest
 tuneGrid <- expand.grid(.mtry = 3)
@@ -179,14 +193,14 @@ modelo_rf <- train(
     x = train_x, y = train_y_factor,
     method = "rf",
     trControl = control,
-    tuneGrid = tuneGrid,
-    ntree = 500
+    #tuneGrid = tuneGrid,
+    #ntree = 500
 )
 predicciones_rf <- modelo_rf$pred
 matrix_confusion_rf <- confusionMatrix(predicciones_rf$pred, predicciones_rf$obs)
 f1_score_rf <- calcular_f1_score(matrix_confusion_rf)
 print(f1_score_rf)
-# 0.983871
+# 0.9746631
 
 # DE LOS 3 TIPOS DE MODELOS EL QUE OBTUVO EL MEJOR F1 SCORE
 # FUE EL RANDOM FOREST
@@ -204,7 +218,7 @@ predicciones_knn <- modelo_knn$pred
 matrix_confusion_knn <- confusionMatrix(predicciones_knn$pred, predicciones_knn$obs)
 f1_score_knn <- calcular_f1_score(matrix_confusion_knn)
 print(f1_score_knn)
-# 0.9817987
+# 0.9700214
 
 # Arboldes de clasificacion
 arbol_decision <- train(
@@ -216,22 +230,23 @@ predicciones_arbol <- arbol_decision$pred
 matrix_confusion_arbol <- confusionMatrix(predicciones_arbol$pred, predicciones_arbol$obs)
 f1_score_arbol <- calcular_f1_score(matrix_confusion_arbol)
 print(f1_score_arbol)
-# 0.9312821
+# 0.9020619
 
 # Random Forest
 tuneGrid <- expand.grid(.mtry = 3)
 modelo_rf <- train(
     x = train_x, y = train_y_factor,
     method = "rf",
-    trControl = control_15,
-    tuneGrid = tuneGrid,
-    ntree = 500
+    trControl = control_15
+    #tuneGrid = tuneGrid,
+    #ntree = 500
 )
+modelo_rf
 predicciones_rf <- modelo_rf$pred
 matrix_confusion_rf <- confusionMatrix(predicciones_rf$pred, predicciones_rf$obs)
 f1_score_rf <- calcular_f1_score(matrix_confusion_rf)
 print(f1_score_rf)
-# 0.983871
+# 0.9757674
 
 # LUEGO DE VALIDAR CON UN K FOLD DE 15 SE VE
 # QUE EL F1 SCORE DE LOS 3 MODELOS CON RESPECTO AL K FOLD 10
@@ -245,14 +260,15 @@ tuneGrid <- expand.grid(.mtry = 3)
 modelo_rf <- train(
     x = train_x, y = train_y_factor,
     method = "rf",
-    tuneGrid = tuneGrid,
-    ntree = 500
+    trControl = control
+    #tuneGrid = tuneGrid,
+    #ntree = 500
 )
 predicciones_rf <- predict(modelo_rf, test_x)
 matrix_confusion_rf <- confusionMatrix(predicciones_rf, test_y_factor)
 f1_score_rf <- calcular_f1_score(matrix_confusion_rf)
 print(f1_score_rf)
-# 0.9655172
+# 0.9777778
 
 # SE OBSERVA UN F1 SCORE QUE CONDICE CON LO ESPERADO
 # RESULTANDO CERCANO AL DE EL ENTRENAMIENTO
@@ -293,13 +309,13 @@ calcular_auc <- function(valid_y_factor, prediccion_knn) {
 }
 # KNN Validacion Cruzada Simple
 modelo_knn <- train(
-    x = train_x, y = train_y_factor,
+    x = rbind(train_x,valid_x), y = c(train_y_factor,valid_y_factor),
     method = "knn"
 )
 prediccion_knn <- predict(modelo_knn, newdata = test_x)
 roc_auc <- calcular_auc(test_y_factor, prediccion_knn)
 print(roc_auc)
-# 0.9887
+# 0.9824
 
 # Random Forest Validacion Cruzada K - Fold 10
 result <- split_cv_fold(wbca, 10)
@@ -316,13 +332,14 @@ modelo_rf <- train(
     x = train_x, y = train_y_factor,
     method = "rf",
     trControl = control,
-    tuneGrid = tuneGrid,
-    ntree = 500
+    # tuneGrid = tuneGrid,
+    # ntree = 500
 )
+modelo_rf
 predicciones_rf <- predict(modelo_rf, test_x)
 roc_auc <- calcular_auc(test_y_factor, predicciones_rf)
 print(roc_auc)
-# 0.9647
+# 0.9577
 
 # COMO METRICA DE RENDIMIENTO ALTERNATIVA USAREMOS EL AUC DE LA CURVA ROC
 # PARA EL MODELO KNN EN VALIDACION CRUZADA SIMPLE OBTUVIMOS UN AUC DE 0.9887
